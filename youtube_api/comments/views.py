6701282122caa5346098bd329c5e3116cd8eece3
justincamp1776 <justincamp1776@gmail.com>
2021-10-13 17:48:26 -0400

    Added CommentList in views and tested get + post in postman

--- youtube_api/comments/views.py
+++ youtube_api/comments/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.http.response import Http404
+from rest_framework.serializers import Serializer
+from .models import Comment
+from .models import Reply
+from .serializers import CommentSerializer
+from .serializers import ReplySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+
 
 # Create your views here.
+class CommentList(APIView):
+
+    def get(self, request):
+        comment = Comment.objects.all()
+        serializer = CommentSerializer(comment, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
